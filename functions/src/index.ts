442a0478e48b1dce899703861c2703e4a3165468
Muhammad Rivki <muhammad.rivki@outlook.com>
2018-06-24 18:07:46 +0700

    🎊 boilerplate finish

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,13 @@
+import * as express from 'express'
 import * as functions from 'firebase-functions';
+
+const app = express()
+
+app.get('/anu', (req, res) => {
+  res.send('Mamam')
+})
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
-});
+exports.app = functions.https.onRequest(app);
