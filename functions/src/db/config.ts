67486c978a5ce8e57e2162cc5082f22921c7ef28
Muhammad Rivki <muhammad.rivki@outlook.com>
2018-06-29 00:51:10 +0700

    add serviceAccount

--- functions/src/db/config.ts
+++ functions/src/db/config.ts
@@ -1,10 +1,11 @@
 // Set the configuration for your app
 // TODO: Replace with your project's config object
 import * as firebase from 'firebase-admin'
+const serviceAccount = require('../../merlin.json');
 
 const config = {
-  credential: firebase.credential.applicationDefault(),
-  databaseURL: "https://merlin-bot.firebaseio.com",
+  credential: firebase.credential.cert(serviceAccount),
+  databaseURL: 'https://merlin-bot.firebaseio.com',
 };
 
 const firebaseApp = firebase.initializeApp(config);
