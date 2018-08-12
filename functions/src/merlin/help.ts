0c0bb92af4b3f79c7a7de2f4495ee1e65b36e161
Muhammad Rivki <muhammad.rivki@outlook.com>
2018-08-12 20:37:10 +0700

    update help

--- functions/src/merlin/help.ts
+++ functions/src/merlin/help.ts
@@ -3,11 +3,11 @@ import { Message } from 'node-telegram-bot-api'
 module.exports = function (bot) {
   bot.onText(/\/help/, async (msg: Message) => {
     const chatId = msg.chat.id
-    await bot.sendMessage(chatId, 'Hai kak.')
-    await bot.sendMessage(chatId, 'Yang bisa Merlin lakuin cuma ini kak.')
+    await bot.sendMessage(chatId, 'Hallo sayangku.')
+    await bot.sendMessage(chatId, 'Yang bisa Merlin lakuin cuma ini say.')
     await bot.sendMessage(chatId, `
-/cek itu nanti Merlin bakal liat siapa aja yang remote.
-/atur itu nanti Merlin bakal ngatur link teamup kaka
+/cek itu nanti Merlin akan ramal siapa aja yang gak di kantor.
+/buat_leave itu kalian bisa set leave tanpa harus buat Team Up lohh. Japri aku aja ya say.
     `)
   })
 }
