5eae1b5325a830e030d9835d4c40b388cfed8a0c
Muhammad Rivki <muhammad.rivki@outlook.com>
2018-08-13 01:39:35 +0700

    new snack

--- functions/src/merlin/snack.ts
+++ functions/src/merlin/snack.ts
@@ -1,5 +1,6 @@
 import { Message } from 'node-telegram-bot-api'
 import { humanizeTodayDate } from '../utils'
+import { day } from '../date-helper'
 
 const PPLS = [
   {
@@ -34,11 +35,25 @@ module.exports = function (bot) {
     const chatId = msg.chat.id
 
     const GROUP_PPL = PPLS[DAY][PPL_VERSION].join('\n')
-    await bot.sendMessage(chatId, 'Oi oiii. Jangan lupa beli snack ya om tante. 😘')
+    await bot.sendMessage(chatId, 'Oi oiii. Jangan lupa yang piket beli snack buat hari ini ya om tante. 😘🤤')
     await bot.sendMessage(chatId, `
 # ${humanizeTodayDate()}
 
 ${GROUP_PPL}
     `)
+  })
+
+  bot.onText(/\/jadwal_piket/, async (msg: Message) => {
+    const chatId = msg.chat.id
+
+    const GROUP_PPL = PPLS.map(p => p['1'].join('\n') + '\n' + p['2'].join('\n'))
+    const MESSAGE = GROUP_PPL.map((PPL, i) => {
+      return `
+# ${day[i + 1]}
+${PPL}
+      `
+    }).join('')
+    bot.sendMessage(chatId, 'Ini jadwal lengkap piketnya ya say. 😍')
+    bot.sendMessage(chatId, MESSAGE)
   })
 }
