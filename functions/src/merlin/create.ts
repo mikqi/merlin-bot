584bec1f7313599f1ad217358ebcf4edaf92413c
Muhammad Rivki <muhammad.rivki@outlook.com>
2018-08-12 20:22:26 +0700

    update

--- functions/src/merlin/create.ts
+++ functions/src/merlin/create.ts
@@ -1,17 +1,16 @@
 import { Message } from 'node-telegram-bot-api'
 import { readUserData } from '../db/user'
 import axios from 'axios'
-import { createEvent } from '../interface'
-import { convertListEvents } from '../utils'
 
 const TYPES = ['group', 'supergroup']
 const URL = 'https://teamup.com/ksba33b1dec7c75214/events'
 
 module.exports = function (bot) {
-  bot.onText(/\/buat_event/, async (msg: Message) => {
+  bot.onText(/\/buat_leave/, async (msg: Message) => {
     const chatId = msg.chat.id
     const type = msg.chat.type
-    const teamupURL = await readUserData(chatId)
+    const todayDate = new Date()
+    const TODAY = `${todayDate.getFullYear()}-${todayDate.getMonth().toString().length > 1 ? todayDate.getMonth() + 1 : '0' + (todayDate.getMonth() + 1)}-${todayDate.getDate()}`
 
     if (TYPES.indexOf(type) >= 0) {
       return bot.sendMessage(chatId, `Japri aku aja dong @${msg.from.username} sayang biar lebih intim. 😘`)
@@ -30,28 +29,29 @@ module.exports = function (bot) {
       reply_markup: JSON.stringify({ force_reply: true }
     )};
 
-    await bot.sendMessage(chatId, 'Masukkan Nama event? (Remote, Cuti, GH, Sick Leave)', opts)
+    await bot.sendMessage(chatId, 'Masukkan Nama event dulu dong say? (Remote, Cuti, GH, Sick Leave)', opts)
       .then( async ({message_id}) => {
         await bot.onReplyToMessage(chatId, message_id, async (responseLabel: Message) => {
           payload.title = responseLabel.text
 
-          await bot.sendMessage(chatId, 'Masukkan tanggal event? (format: YYYY-MM-DD)', opts)
+          await bot.sendMessage(chatId, `Mulai tanggal berapa nih? Sekarang tuh tanggal ${TODAY}. (format: YYYY-MM-DD)`, opts)
             .then( async ({message_id: messageId}) => {
               await bot.onReplyToMessage(chatId, messageId, async (responseDate: Message) => {
                 payload.start_dt = responseDate.text
 
-                await bot.sendMessage(chatId, 'Masukkan tanggal masuk? (format: YYYY-MM-DD)', opts)
+                await bot.sendMessage(chatId, 'Mau balik lagi ke kantor kapan say? (format: YYYY-MM-DD)', opts)
                   .then( async ({message_id: messageId}) => {
                     await bot.onReplyToMessage(chatId, messageId, async (responseEndDate: Message) => {
+                      bot.sendMessage(chatId, 'Oke sebentar say, Merlin tambahin dulu ya.. 🏃🏻‍')
                       payload.end_dt = responseEndDate.text
                       const HEADERS = { headers: { 'Teamup-Token': process.env.API_KEY }}
 
                       axios.post(URL, payload, HEADERS)
                         .then(_ => {
-                          bot.sendMessage(chatId, `Berhasil menambahkan pada tanggal ${payload.start_dt} - ${payload.end_dt}`)
+                          bot.sendMessage(chatId, `Yeiyyy kamu sudah menambahkan ${payload.title} untuk tanggal ${payload.start_dt} sampai ${payload.end_dt}. Take care baby. 😘`)
                         })
                         .catch(_ => {
-                          bot.sendMessage(chatId, 'Gagal menambahkan ke Teamup')
+                          bot.sendMessage(chatId, 'Aduhhh.. Kayanya ada yang salah deh 😢 Coba lagi atau bisa tanya ke bro @mikqi atau bro @gazandi yaa. 😘')
                         })
                     
                     })
