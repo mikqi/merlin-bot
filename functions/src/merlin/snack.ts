c57de9c2446edf18119a767297ee4bf70ba96926
Muhammad Rivki <muhammad.rivki@outlook.com>
2018-08-13 09:47:27 +0700

    refactor snack command

--- functions/src/merlin/snack.ts
+++ functions/src/merlin/snack.ts
@@ -1,37 +1,11 @@
 import { Message } from 'node-telegram-bot-api'
 import { humanizeTodayDate } from '../utils'
 import { day } from '../date-helper'
-
-const PPLS = [
-  {
-    '1': ['@adityafgan', '@firliadyta', '@irfanalfarabbi', '@akbarstd'],
-    '2': ['@rezafaizarahman', '@danielbintar', '@elelvyra']
-  },
-  {
-    '1': ['@kevingoz', '@vgeraldo', '@fikryd14'],
-    '2': ['@gazandi', '@hamnahs', '@Apreliamaisara']
-  },
-  {
-    '1': ['@mrqorib', '@vandyroy', '@noviyaniw'],
-    '2': ['@dhinazor', '@izzuddin012', '@tenosiswono']
-  },
-  {
-    '1': ['@KevinYudistira', '@handrata', '@calvingzhartono'],
-    '2': ['@charlesrandicha', '@brurce', '@msabiqd']
-  },
-  {
-    '1': ['@mikqi', '@erjodi', '@oliviaputr'],
-    '2': ['@arkadiusreymond', '@herwando', '@albab21']
-  }
-]
+import { PPLS, PPL_VERSION, DAY } from '../ppl-utils'
 
 module.exports = function (bot) {
-  const DATES = new Date()
-  const DATE = DATES.getDate()
-  const DAY = DATES.getDay() - 1
-  const PPL_VERSION = DATE % 2 !== 0 ? '1' : '2'
 
-  bot.onText(/\/snack_today/, async (msg: Message) => {
+  bot.onText(/\/piket_hari_ini/, async (msg: Message) => {
     const chatId = msg.chat.id
 
     const GROUP_PPL = PPLS[DAY][PPL_VERSION].join('\n')
