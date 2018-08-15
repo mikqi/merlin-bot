77ae5dd38b3750bf47707412ed128e0e2ffe6669
Muhammad Rivki <muhammad.rivki@outlook.com>
2018-08-15 10:45:58 +0700

    update copywrite

--- functions/src/merlin/done.ts
+++ functions/src/merlin/done.ts
@@ -7,7 +7,7 @@ module.exports = function (bot) {
     const type = msg.chat.type
 
     if (TYPES.indexOf(type) >= 0) {
-      await bot.sendMessage(chatId, `Yeiyyyy si @${msg.from.username} sudah beli snack lohhhh.. Makasih loh yaa @${msg.from.username} sayanggg 😘. Jangan lupa dihabiskan ya. Yang udah gemuk gak usah makan snack biar yang belum gemuk aja yang makan. 🙊`)
+      await bot.sendMessage(chatId, `Yeiyyyy si @${msg.from.username} sudah beli snack lohhhh.. Makasih yaaa sayanggg 😘. Jangan lupa dihabiskan ya teman-teman..`)
     } else {
       await bot.sendMessage(chatId, 'Ngapain sih om? Ampas lu om. Udah beli apa coba?')
     }
