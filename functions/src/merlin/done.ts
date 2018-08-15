0b85c8115d8fa0994ee5d9038f73f4ae860c8226
Muhammad Rivki <muhammad.rivki@outlook.com>
2018-08-15 10:26:38 +0700

    add validation

--- functions/src/merlin/done.ts
+++ functions/src/merlin/done.ts
@@ -2,7 +2,16 @@ import { Message } from 'node-telegram-bot-api'
 
 module.exports = function (bot) {
   bot.onText(/\/udah_beli/, async (msg: Message) => {
+    const TYPES = ['group', 'supergroup']
     const chatId = msg.chat.id
-    await bot.sendMessage(chatId, `Yeiyyyy si @${msg.from.username} sudah beli snack lohhhh.. Makasih loh yaa @${msg.from.username} sayanggg 😘. Jangan lupa dihabiskan ya. Yang udah gemuk gak usah makan snack biar yang belum gemuk aja yang makan. 🙊`)
+    const type = msg.chat.type
+
+    if (TYPES.indexOf(type) >= 0) {
+      await bot.sendMessage(chatId, `Yeiyyyy si @${msg.from.username} sudah beli snack lohhhh.. Makasih loh yaa @${msg.from.username} sayanggg 😘. Jangan lupa dihabiskan ya. Yang udah gemuk gak usah makan snack biar yang belum gemuk aja yang makan. 🙊`)
+    } else {
+      await bot.sendMessage(chatId, 'Ngapain sih om? Ampas lu om. Udah beli apa coba?')
+    }
+
   })
+
 }
