0b85014148cf9f983954c91efb289c495dc770fe
Gazandi <gazandic@gmail.com>
2018-09-05 00:12:52 +0700

    fix snack algorithm

--- functions/src/merlin/snack.ts
+++ functions/src/merlin/snack.ts
@@ -7,9 +7,12 @@ module.exports = function (bot) {
 
   bot.onText(/\/piket_hari_ini/, async (msg: Message) => {
     const chatId = msg.chat.id
-    const DATES = new Date()
-    const DATE = DATES.getDate()
     const DAY = DATES.getDay() - 1
+    const DATES = new Date()
+    const START = new Date(2018, 0, 0)
+    const DIFF = (DATES - START) + ((START.getTimezoneOffset() - DATES.getTimezoneOffset()) * 60 * 1000)
+    const ONEDAY = 1000 * 60 * 60 * 24;
+    const DATE = Math.floor(DIFF / ONEDAY);
     const PPL_VERSION = DATE % 2 !== 0 ? '1' : '2'
 
     const GROUP_PPL = PPLS[DAY][PPL_VERSION].join('\n')
