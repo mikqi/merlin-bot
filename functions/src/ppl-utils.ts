42141a856af56f311dd4f14758c937e7d2d47496
Muhammad Rivki <muhammad.rivki@outlook.com>
2018-09-26 10:28:15 +0700

    update piket

--- functions/src/ppl-utils.ts
+++ functions/src/ppl-utils.ts
@@ -1,22 +1,27 @@
 export const PPLS = [
   {
-    '1': ['@adityafgan', '@firliadyta', '@irfanalfarabbi', '@akbarstd'],
-    '2': ['@rezafaizarahman', '@danielbintar', '@elelvyra']
+    '1': ['@adityafgan', '@firliadyta', '@mfirmannur'],
+    '2': ['@rezafaizarahman', '@danielbintar', '@elelvyra', '@odheta']
   },
   {
-    '1': ['@kevingoz', '@vgeraldo', '@fikryd14'],
-    '2': ['@gazandi', '@hamnahs', '@Apreliamaisara']
+    '1': ['@kevingoz', '@vgeraldo', '@fikryd14', '@puputivana'],
+    '2': ['@gazandi', '@hamnahs', '@Apreliamaisara', '@ditasav']
   },
   {
-    '1': ['@mrqorib', '@vandyroy', '@noviyaniw'],
-    '2': ['@dhinazor', '@izzuddin012', '@tenosiswono']
+    '1': ['@mrqorib', '@vandyroy',  '@aswindapp'],
+    '2': ['@dhinazor', '@mikqi', '@tenosiswono']
   },
   {
     '1': ['@KevinYudistira', '@handrata', '@calvingzhartono'],
-    '2': ['@charlesrandicha', '@brurce', '@msabiqd']
+    '2': ['@charlesrandicha', '@brurce', '@msabiqd', '@atikazzahra']
   },
   {
-    '1': ['@mikqi', '@erjodi', '@oliviaputr'],
+    '1': ['@noviyaniw', '@oliviaputr', '@akbarstd'],
     '2': ['@arkadiusreymond', '@herwando', '@albab21']
   }
 ]
+
+export const DATES = new Date()
+export const DATE = DATES.getDate()
+export const DAY = DATES.getDay() - 1
+export const PPL_VERSION = DATE % 2 !== 0 ? '1' : '2'
