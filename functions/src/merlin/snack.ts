3060a9318b952128be71c0f40bbb7afb25d46ec5
Gazandi <gazandic@gmail.com>
2018-11-08 02:56:44 +0700

    swap snack

--- functions/src/merlin/snack.ts
+++ functions/src/merlin/snack.ts
@@ -13,7 +13,7 @@ module.exports = function (bot) {
     const DIFF = (DATES - START) + ((START.getTimezoneOffset() - DATES.getTimezoneOffset()) * 60 * 1000)
     const ONEDAY = 1000 * 60 * 60 * 24;
     const DATE = Math.floor(DIFF / ONEDAY);
-    const PPL_VERSION = DATE % 2 !== 0 ? '1' : '2'
+    const PPL_VERSION = DATE % 2 !== 0 ? '2' : '1'
 
     const GROUP_PPL = PPLS[DAY][PPL_VERSION].join('\n')
     await bot.sendMessage(chatId, 'Oi oiii. Jangan lupa yang piket beli snack buat hari ini ya om tante. Ini demi kebersamaan umat ya sayy...😘🤤')
