7767a83db509d8ea647afac5a0a64d41ebe2ba73
Gazandi <gazandic@gmail.com>
2018-11-08 02:57:49 +0700

    swap ppl_version

--- functions/src/job.ts
+++ functions/src/job.ts
@@ -65,7 +65,7 @@ const piket = async function (chatId, bot) {
   const DIFF = (DATES - START) + ((START.getTimezoneOffset() - DATES.getTimezoneOffset()) * 60 * 1000)
   const ONEDAY = 1000 * 60 * 60 * 24;
   const DATE = Math.floor(DIFF / ONEDAY);
-  const PPL_VERSION = DATE % 2 !== 0 ? '1' : '2'
+  const PPL_VERSION = DATE % 2 !== 0 ? '2' : '1'
 
   const GROUP_PPL = PPLS[DAY][PPL_VERSION].join('\n')
   await bot.sendMessage(chatId, 'Oi oiii. Jangan lupa yang piket beli snack buat hari ini ya om tante. Ini demi kebersmaan umat ya sayy...😘🤤')
